1e7a63c8e1316c08a10d624d73ef6fd64c58a4f8
Robin1238 <robin1238soypro@gmail.com>
2025-12-24 05:31:13 -0500

    fix v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://Robin1238.github.io/CYAC",
-  base: "/CYAC/",
+  // site: "https://Robin1238.github.io/CYAC",
+  // base: "/CYAC/",
 });
